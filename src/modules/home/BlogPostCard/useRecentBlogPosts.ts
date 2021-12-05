ec29bfdd2fff22ca23575442362ad687ff95da67
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-05 08:39:45 -0500

    scaffold

--- src/modules/home/BlogPostCard/useRecentBlogPosts.ts
+++ src/modules/home/BlogPostCard/useRecentBlogPosts.ts
@@ -8,11 +8,13 @@ export interface BlogPost extends Source {
 
 const fetcher = (args: { limit: number }) => {
 	const _inner: Fetcher<BlogPost[]> = async () => {
-		const githubSources = await findSourcesByType('github-repo').then((res) => res.slice(0, args.limit))
+		const blogPostSources = await findSourcesByType('blog-post')
+		const tutorialSources = await findSourcesByType('tutorial')
+		const res = [...tutorialSources, ...blogPostSources].slice(0, args.limit)
 
 		// find last updated dates for each repo
 		// mocked for now
-		return githubSources.map((source) => ({ ...source, updatedAt: new Date().toDateString() }))
+		return res.map((source) => ({ ...source, updatedAt: new Date().toDateString() }))
 	}
 
 	return _inner
