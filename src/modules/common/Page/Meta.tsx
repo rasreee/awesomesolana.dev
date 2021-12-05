d499ae5a42f67a600cde56cc159647d57a664d10
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-05 08:33:21 -0500

    replace name

--- src/modules/common/Page/Meta.tsx
+++ src/modules/common/Page/Meta.tsx
@@ -19,19 +19,19 @@ export const Meta: React.FunctionComponent<MetaProps> = ({ description, title, i
 			<meta name="description" content={description} />
 			<link rel="manifest" href="/site.webmanifest" />
 			<meta name="theme-color" content={theme.colors.primary} />
-			<meta property="og:title" content={title + ' - @rasreee'} />
+			<meta property="og:title" content={title + ' - Awesome Solana'} />
 			<meta property="og:description" content={description} />
 			<meta property="og:image" content={image} />
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta name="twitter:description" content={description} />
-			<meta name="twitter:title" content={title + ' - @rasreee'} />
+			<meta name="twitter:title" content={title + ' - Awesome Solana'} />
 			<meta name="twitter:image" content={image} />
 			<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
 			<link rel="mask-icon" href={safariPinnedTab} color={theme.colors.primary} />
 			<link rel="icon" type="image/png" sizes="32x32" href={favIcon32} />
 			<link rel="icon" type="image/png" sizes="16x16" href={favIcon16} />
 			<link rel="shortcut icon" href={favIcon} />
-			<title>{title + ' - @rasreee'}</title>
+			<title>{title + ' - Awesome Solana'}</title>
 		</Head>
 	)
 }
