e675020f24dfa2cc892b7e7c253692184bdb8cfb
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-06 03:40:07 -0500

    add source type tag to header

--- src/modules/common/Tag.tsx
+++ src/modules/common/Tag.tsx
@@ -1,28 +1,34 @@
 import classNames from 'classnames'
+import { darken, lighten } from 'polished'
 import React from 'react'
 
+import { theme } from '@/theme/theme'
+import { FontSize, ThemeColorName } from '@/ui/foundations'
+import { pseudo } from '@/ui/helpers'
+
 export interface TagProps {
-	color?: string
+	color?: ThemeColorName
+	fontSize?: FontSize
 }
 
-export const Tag: React.FunctionComponent<TagProps> = ({ children, color = 'teal', ...props }) => {
-	const bgClasses = `bg-${color}-100 active:bg-${color}-200`
-
-	const txtColorClasses = `text-${color}-600 active:text-${color}-700`
-
-	const borderColorClasses = `hover:border-${color}-300`
-
+export const Tag: React.FunctionComponent<TagProps> = ({
+	children,
+	color = 'turquoise',
+	fontSize = 'xs',
+	...props
+}) => {
 	return (
 		<div
-			className={classNames(
-				'rounded px-2 py-0.5',
-				'font-medium leading-tight',
-				bgClasses,
-				txtColorClasses,
-				`hover:shadow-sm hover:border`,
-				borderColorClasses,
-				'cursor-pointer'
-			)}
+			className={classNames('rounded px-2 py-0.5', 'font-medium leading-tight', `hover:shadow-sm`, 'cursor-pointer')}
+			css={{
+				fontSize: theme.fontSizes[fontSize],
+				background: lighten('0.325', theme.colors[color]),
+				color: darken('0.2', theme.colors[color]),
+				[pseudo('_hover')]: {
+					borderWidth: '1.5px',
+					borderColor: darken('0.1', theme.colors[color])
+				}
+			}}
 			{...props}
 		>
 			{children}
