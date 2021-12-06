e5d25a1762c5477d1e791615191b010083bf91b8
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-06 06:23:58 -0500

    integrate with next-seo

--- src/common/components/Page/Meta.tsx
+++ src/common/components/Page/Meta.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
-import React from 'react'
+import { NextSeo } from 'next-seo'
+import React, { useMemo } from 'react'
 
+import { AppConfig } from '@/common/utils/AppConfig'
 import { theme } from '@/theme/theme'
 
 import { appleTouchIcon, favIcon, favIcon16, favIcon32, safariPinnedTab } from './constants'
@@ -9,29 +11,47 @@ export type MetaProps = {
 	title: string
 	description: string
 	image?: string
+	canonical?: string
 }
 
-export const Meta: React.FunctionComponent<MetaProps> = ({ description, title, image }) => {
+export const Meta: React.FunctionComponent<MetaProps> = ({ description, title, image, canonical }) => {
+	const titleText = useMemo(() => title + ' - Awesome Solana', [title])
+
 	return (
-		<Head>
-			<meta httpEquiv="" content="IE=edge" />
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-			<meta name="description" content={description} />
-			<link rel="manifest" href="/site.webmanifest" />
-			<meta name="theme-color" content={theme.colors.primary} />
-			<meta property="og:title" content={title + ' - Awesome Solana'} />
-			<meta property="og:description" content={description} />
-			<meta property="og:image" content={image} />
-			<meta name="twitter:card" content="summary_large_image" />
-			<meta name="twitter:description" content={description} />
-			<meta name="twitter:title" content={title + ' - Awesome Solana'} />
-			<meta name="twitter:image" content={image} />
-			<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
-			<link rel="mask-icon" href={safariPinnedTab} color={theme.colors.primary} />
-			<link rel="icon" type="image/png" sizes="32x32" href={favIcon32} />
-			<link rel="icon" type="image/png" sizes="16x16" href={favIcon16} />
-			<link rel="shortcut icon" href={favIcon} />
-			<title>{title + ' - Awesome Solana'}</title>
-		</Head>
+		<>
+			<Head>
+				<meta charSet="UTF-8" key="charset" />
+				<meta httpEquiv="" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" key="viewport" />
+				<meta name="description" content={description} />
+				<link rel="manifest" href="/site.webmanifest" />
+				<meta name="theme-color" content={theme.colors.primary} />
+				<meta property="og:title" content={titleText} />
+				<meta property="og:description" content={description} />
+				<meta property="og:image" content={image} />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:description" content={description} />
+				<meta name="twitter:title" content={titleText} />
+				<meta name="twitter:image" content={image} />
+				<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+				<link rel="mask-icon" href={safariPinnedTab} color={theme.colors.primary} />
+				<link rel="icon" type="image/png" sizes="32x32" href={favIcon32} />
+				<link rel="icon" type="image/png" sizes="16x16" href={favIcon16} />
+				<link rel="shortcut icon" href={favIcon} />
+				<title>{titleText}</title>
+			</Head>
+			<NextSeo
+				title={title}
+				description={description}
+				canonical={canonical}
+				openGraph={{
+					title: titleText,
+					description,
+					url: canonical,
+					locale: AppConfig.locale,
+					site_name: AppConfig.site_name
+				}}
+			/>
+		</>
 	)
 }
---
+++ src/common/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+	site_name: 'Awesome Solana Dev',
+	title: 'Awesome Solana Dev',
+	description: 'Awesome Solana development resources including real-world codebases, tutorials, and more.',
+	locale: 'en'
+}
