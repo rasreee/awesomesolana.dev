1015bf2876d259f0892a47ba922c24b7bf564519
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-13 20:23:51 -0500

    fix: type error

--- src/models/source/useUpdateSourceData.ts
+++ src/models/source/useUpdateSourceData.ts
@@ -1,21 +1,21 @@
 import { useSupabase } from '@/common/supabase/useSupabase'
 import { handleSupabaseResponse } from '@/common/utils/handleSupabaseResponse'
 
-import { Source } from './types'
+import { RawSource } from './types'
 import { useSourceById } from './useSourceById'
 
 export const useUpdateSourceData = (id: string) => {
 	const supabase = useSupabase()
 	const { data: source, mutate: mutateSource } = useSourceById(id)
 
-	const request = async (id: string, data: Partial<Omit<Source, 'id' | 'tags'>>) => {
+	const request = async (id: string, data: Partial<Omit<RawSource, 'id' | 'tags'>>) => {
 		if (!source) {
 			throw new Error('tried to update source likes when source data was not available yet')
 		}
 
 		const newSourcesData = { ...source, ...data }
 
-		await supabase.from<string>('sources').update(data).match({ id }).then(handleSupabaseResponse)
+		await supabase.from<RawSource>('sources').update(data).match({ id }).then(handleSupabaseResponse)
 
 		await mutateSource(newSourcesData)
 
