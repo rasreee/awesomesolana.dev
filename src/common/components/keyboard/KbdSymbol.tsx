0f33fc737b2f3649f83836a9b4527a1091f45b3a
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-20 18:44:10 -0500

    migrate KbdSymbol to emotion

--- src/common/components/keyboard/KbdSymbol.tsx
+++ src/common/components/keyboard/KbdSymbol.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
+import styled from '@/styled'
+
 import { eventKbdAbbrs, isEventKey, KeyboardKey } from './keys'
+
+const KbdSymbolItemAbbr = styled.abbr`
+	text-decoration-line: none;
+	color: var(--gray-300);
+`
 
 export const KbdSymbolItem = ({ value }: { value: KeyboardKey }) => {
 	if (isEventKey(value)) {
@@ -10,11 +17,15 @@ export const KbdSymbolItem = ({ value }: { value: KeyboardKey }) => {
 			console.warn(`KbdSymbolItem was passed unknown value ${abbrVal}`)
 		}
 
-		return <abbr className="no-underline text-gray-300 dark:text-gray-500">{abbrVal}</abbr>
+		return <KbdSymbolItemAbbr>{abbrVal}</KbdSymbolItemAbbr>
 	} else {
 		return <>{value}</>
 	}
 }
+
+const Kbd = styled.kbd`
+	font-weight: var(--font-semibold);
+`
 
 export interface KbdSymbolProps {
 	keys: KeyboardKey[]
@@ -22,10 +33,10 @@ export interface KbdSymbolProps {
 
 export const KbdSymbol: React.FunctionComponent<KbdSymbolProps> = ({ keys }) => {
 	return (
-		<kbd className="font-sans font-semibold dark:text-gray-500">
+		<Kbd>
 			{keys.map((kbdItem) => (
 				<KbdSymbolItem key={kbdItem} value={kbdItem} />
 			))}
-		</kbd>
+		</Kbd>
 	)
 }
