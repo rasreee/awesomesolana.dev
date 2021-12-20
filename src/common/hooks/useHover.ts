cb7a21c3356c3e0a4f0a0dbf5e4dfc1d414931f8
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-20 17:14:19 -0500

    fix type error

--- src/common/hooks/useHover.ts
+++ src/common/hooks/useHover.ts
@@ -1,8 +1,8 @@
-import { MutableRefObject, Ref, useEffect, useRef, useState } from 'react'
+import { Ref, useEffect, useRef, useState } from 'react'
 
 export function useHover<T extends HTMLElement>(): { isHovered: boolean; bind: { ref: Ref<T> } } {
 	const [value, setValue] = useState<boolean>(false)
-	const ref: any = useRef<T | null>(null)
+	const ref = useRef<T | null>(null)
 	const handleMouseOver = (): void => setValue(true)
 	const handleMouseOut = (): void => setValue(false)
 
