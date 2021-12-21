121e9c7868728bd5935be68c9d38e41dfa57fd8c
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-20 22:18:44 -0500

    organize dropdown result by source type

--- src/modules/search/SearchDropdown/SearchDropdown.tsx
+++ src/modules/search/SearchDropdown/SearchDropdown.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
-import { Source } from '@/models/source/types'
+import { formatSourceTypeLabel } from '@/models/source'
+import { Source, SOURCE_TYPES, SourceType } from '@/models/source/types'
 
 import { SearchHitsData } from '../types'
 import * as S from './SearchDropdown.styles'
+
+export type SearchDropdownSectionProps = {
+	type: SourceType | 'recents'
+	items: Source[]
+	renderItem: (hit: Source, index: number) => JSX.Element
+}
+
+export const SearchDropdownSection: React.FunctionComponent<SearchDropdownSectionProps> = ({
+	type,
+	items,
+	renderItem,
+	...props
+}) => {
+	if (items.length === 0) return null
+
+	return (
+		<S.Content {...props}>
+			<S.Section>
+				<S.SectionTitle>{type === 'recents' ? 'Recents' : formatSourceTypeLabel(type)}</S.SectionTitle>
+				<S.List role="listbox">
+					{items.map((hit, index) => (
+						<S.ListItem key={hit.id}>{renderItem(hit, index)}</S.ListItem>
+					))}
+				</S.List>
+			</S.Section>
+		</S.Content>
+	)
+}
 
 export interface SearchDropdownProps {
 	data: SearchHitsData
@@ -16,16 +45,14 @@ export const SearchDropdown: React.FunctionComponent<SearchDropdownProps> = ({ s
 
 	return (
 		<S.Dropdown>
-			<S.Content>
-				<S.Section>
-					{data.type === 'recents' && <S.SectionTitle>Recents</S.SectionTitle>}
-					<S.List role="listbox">
-						{data.list.map((hit, index) => (
-							<S.ListItem key={hit.id}>{renderItem(hit, index)}</S.ListItem>
-						))}
-					</S.List>
-				</S.Section>
-			</S.Content>
+			{SOURCE_TYPES.map((sourceType) => (
+				<SearchDropdownSection
+					key={sourceType}
+					type={sourceType}
+					items={data.list.filter((item) => item.type === sourceType)}
+					renderItem={renderItem}
+				/>
+			))}
 		</S.Dropdown>
 	)
 }
