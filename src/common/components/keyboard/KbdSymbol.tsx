3e7a53475361c7d3ee3ab3fc8de993a73a8a6d44
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-21 07:46:09 -0500

    clean

--- src/common/components/keyboard/KbdSymbol.tsx
+++ src/common/components/keyboard/KbdSymbol.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import styled from '@/styled'
+
 import { EventKey, isEventKey, KeyboardKey } from './keys'
 
 const eventKbdAbbrs: Partial<{ [k in EventKey]: string }> = {
@@ -7,21 +9,29 @@ const eventKbdAbbrs: Partial<{ [k in EventKey]: string }> = {
 	Escape: 'esc'
 }
 
-export const KbdSymbolItem = ({ id }: { id: KeyboardKey }) => {
-	let abbrValue
+const KbdSymbolItemAbbr = styled.abbr`
+	-webkit-text-decoration-line: none;
+	text-decoration-line: none;
+`
 
-	if (isEventKey(id)) {
-		if (!Object.keys(eventKbdAbbrs).includes(id)) {
-			console.warn(`KbdSymbolItem was passed unknown id ${id}`)
+export const KbdSymbolItem = ({ value }: { value: KeyboardKey }) => {
+	if (isEventKey(value)) {
+		const abbrVal = eventKbdAbbrs[value] ?? ''
+
+		if (!Object.keys(eventKbdAbbrs).includes(value)) {
+			console.warn(`KbdSymbolItem was passed unknown value ${abbrVal}`)
 		}
 
-		abbrValue = Object.keys(eventKbdAbbrs).includes(id) ? eventKbdAbbrs[id] : ''
+		return <KbdSymbolItemAbbr>{abbrVal}</KbdSymbolItemAbbr>
+	} else {
+		return <>{value}</>
 	}
-
-	abbrValue ??= id
-
-	return <abbr className="no-underline">{abbrValue}</abbr>
 }
+
+const Kbd = styled.kbd`
+	font-weight: var(--font-semibold);
+	color: var(--gray-300);
+`
 
 export interface KbdSymbolProps {
 	keys: KeyboardKey[]
@@ -29,10 +39,10 @@ export interface KbdSymbolProps {
 
 export const KbdSymbol: React.FunctionComponent<KbdSymbolProps> = ({ keys }) => {
 	return (
-		<kbd className="font-sans font-semibold text-gray-300 dark:text-gray-500">
+		<Kbd>
 			{keys.map((kbdItem) => (
-				<KbdSymbolItem key={kbdItem} id={kbdItem} />
+				<KbdSymbolItem key={kbdItem} value={kbdItem} />
 			))}
-		</kbd>
+		</Kbd>
 	)
 }
