fffe5beb0ab55965bcd445cb5d592450ee12e9ab
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-02-16 14:46:15 -0500

    feat: fix layout

--- src/ui/layout/Layout.tsx
+++ src/ui/layout/Layout.tsx
@@ -2,16 +2,20 @@ import * as React from 'react';
 
 import Seo from '@/ui/Seo';
 
+import Footer from './Footer';
+import Header from './Header';
+
 export interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  // Put Header or Footer Here
   return (
     <>
       <Seo />
-      {children}
+      <Header />
+      <main className="min-h-[calc(100vh-124px)]">{children}</main>
+      <Footer />
     </>
   );
 }
