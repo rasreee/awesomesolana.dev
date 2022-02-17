5035954a4a0353bac67bb09a7e6780e980d67f38
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-02-16 19:10:01 -0500

    fix: responsiveness

--- src/ui/layout/LogoLink.tsx
+++ src/ui/layout/LogoLink.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
 
-import { useIsMobile } from '@/hooks/useIsMobile';
 import clsxm from '@/lib/clsxm';
 import UnstyledLink from '@/ui/links/UnstyledLink';
 
 function LogoLink() {
-  const isMobile = useIsMobile();
-
-  const logoText = React.useMemo(
-    () => (isMobile ? 'AWESOL' : 'AWESOME SOLANA'),
-    [],
-  );
-
   return (
-    <UnstyledLink
-      href="/"
-      className={clsxm(
-        'max-w-min sm:min-w-max',
-        'font-sans text-base font-extrabold leading-none hover:text-gray-600 sm:text-xl',
-      )}
-    >
-      {logoText}
-    </UnstyledLink>
+    <>
+      <div className="hidden md:block">
+        <UnstyledLink
+          href="/"
+          className={clsxm(
+            'max-w-min sm:min-w-max',
+            'font-sans text-xl font-extrabold leading-none hover:text-gray-600',
+          )}
+        >
+          AWESOME SOLANA
+        </UnstyledLink>
+      </div>
+      <div className="md:hidden">
+        <UnstyledLink
+          href="/"
+          className={clsxm(
+            'max-w-min sm:min-w-max',
+            'font-sans text-base text-xl font-extrabold leading-none hover:text-gray-600',
+          )}
+        >
+          AWESOME SOLANA
+        </UnstyledLink>
+      </div>
+    </>
   );
 }
 
