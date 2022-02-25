c3adfc83cd1d7b399854bf1c9e7d669f4cb3a7e1
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-02-24 23:55:36 -0500

    chore: fix

--- src/modules/search/SearchPage.tsx
+++ src/modules/search/SearchPage.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
+
 import {
   ALL_PROJECTS,
   filterProjectsByTags,
   filterProjectsByTitle,
   Project,
 } from '@/api/projects';
+import { useSearchOptions } from '@/contexts/AppContext';
 import { useSearchFilters } from '@/contexts/SearchContext';
+import clsxm from '@/lib/clsxm';
 import { Layout, SearchField, useSearchField } from '@/ui/components';
-import { SearchOptionsMenuToggle } from '@/ui/components/SearchOptionsMenuToggle';
+import { AdjustmentsIcon } from '@/ui/icons';
 
 import { Results } from './Results';
 
@@ -32,5 +36,24 @@ export function SearchPage() {
         <Results hits={searchField.hits} />
       </div>
     </Layout>
+  );
+}
+
+function SearchOptionsMenuToggle() {
+  const { isOpen, toggle } = useSearchOptions();
+
+  return (
+    <button
+      onClick={toggle}
+      className={clsxm(
+        'text',
+        isOpen && 'bg-surface text-color-primary',
+        'h-full rounded p-1',
+      )}
+    >
+      <AdjustmentsIcon
+        className={clsxm('text-hint', isOpen && 'text-color-primary')}
+      />
+    </button>
   );
 }
