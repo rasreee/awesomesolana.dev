ac4618e1a9f7170a0d403f076d3d6e1f4716efb6
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-02-25 18:26:58 -0500

    fix: typings

--- src/modules/github/useGithubReposApi.ts
+++ src/modules/github/useGithubReposApi.ts
@@ -8,16 +8,18 @@ import {
 import { parseRawGitHubRepo } from './helpers';
 import { GitHubApiResponse, GitHubRepo } from './types';
 
+export type UseGithubReposApi = {
+  data: GitHubRepo[] | undefined;
+  error: Error | undefined;
+};
+
 export function useGithubReposApi<Route extends '/search' | '/browse'>(
   route: Route,
   params?: Route extends '/search'
     ? Partial<GithubReposSearchParams>
     : Partial<GithubReposBrowseParams>,
   shouldFetch = true,
-): {
-  data: GitHubRepo[] | undefined;
-  error: Error | undefined;
-} {
+): UseGithubReposApi {
   const { data: rawData, error } = useSWR<GitHubApiResponse, Error>(
     shouldFetch ? githubSwrKey.route(route, params) : null,
   );
@@ -29,18 +31,12 @@ export function useGithubReposApi<Route extends '/search' | '/browse'>(
 
 export function useBrowseGithubRepos(
   params?: Partial<GithubReposBrowseParams>,
-): {
-  data: GitHubRepo[] | undefined;
-  error: Error | undefined;
-} {
+): UseGithubReposApi {
   return useGithubReposApi('/browse', params);
 }
 
 export function useSearchGithubRepos(
   params?: Partial<GithubReposSearchParams>,
-): {
-  data: GitHubRepo[] | undefined;
-  error: Error | undefined;
-} {
+): UseGithubReposApi {
   return useGithubReposApi('/search', params);
 }
