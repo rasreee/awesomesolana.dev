3576678a7dbafca1d83df2ac91ed20d84d098d75
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-02-25 04:08:02 -0500

    refactor: projects

--- src/modules/projects/api.ts
+++ src/modules/projects/api.ts
@@ -1,7 +1,64 @@
+import useSWR, { SWRResponse } from 'swr';
+
+import { getIntersection, uniques } from '@/common/utils';
+
 import { Tag } from '../tags';
 import { ALL_PROJECTS } from './constants';
-import { filterProjectsByTags, filterProjectsByTitle } from './helpers';
 import { Project } from './types';
+
+export function filterProjectsByTitle(
+  projects: Project[],
+  query: string,
+): Project[] {
+  if (!query) return projects;
+
+  let hits = [] as Project[];
+
+  const a = query.toLowerCase();
+
+  hits = projects.filter((item) => {
+    const name = item.title;
+    const b = name.toLowerCase().slice(0, query.length);
+    return a === b;
+  });
+
+  return hits;
+}
+
+function isRelevantProject(project: Project, tags: Tag[]): boolean {
+  const projectTagTypes = uniques(
+    project.tags.map((project) => project.category),
+  );
+
+  const commonTags: Tag[] = [];
+  projectTagTypes.forEach((category) => {
+    const projectTags = project.tags.filter((tag) => tag.category === category);
+    const tagsForType = tags.filter((tag) => tag.category === category);
+
+    const intersection = getIntersection(
+      projectTags,
+      tagsForType,
+      (a, b) => a.name === b.name,
+    );
+
+    commonTags.push(...intersection);
+  });
+
+  return commonTags.length > 0;
+}
+
+export function filterProjectsByTags(
+  projects: Project[],
+  tags: Tag[],
+): Project[] {
+  if (tags.length === 0) return projects;
+
+  const filtered = projects.filter((project) =>
+    isRelevantProject(project, tags),
+  );
+
+  return filtered;
+}
 
 export async function searchProjects(
   query: string,
@@ -11,4 +68,21 @@ export async function searchProjects(
   const result = filterProjectsByTitle(initialResult, query);
 
   return result;
+}
+
+export function useProjectsByTags(
+  tags: Tag[] | undefined,
+): SWRResponse<Project[], Error> {
+  const swr = useSWR<Project[], Error>(
+    tags
+      ? `projects?tags=${encodeURIComponent(
+          tags.map((tag) => tag.name).join(','),
+        )}`
+      : null,
+    async (): Promise<Project[]> => {
+      return filterProjectsByTags(ALL_PROJECTS, tags ?? []);
+    },
+  );
+
+  return swr;
 }
--- src/modules/projects/helpers.ts
+++ src/modules/projects/helpers.ts
@@ -1,94 +1,12 @@
-import useSWR, { SWRResponse } from 'swr';
-
-import { getIntersection, uniques } from '@/common/utils';
 import { Tag } from '@/modules/tags';
 
 import { ALL_PROJECTS } from './constants';
-import { Project } from './types';
-
-export function filterProjectsByTitle(
-  projects: Project[],
-  query: string,
-): Project[] {
-  if (!query) return projects;
-
-  let hits = [] as Project[];
-
-  const a = query.toLowerCase();
-
-  hits = projects.filter((item) => {
-    const name = item.title;
-    const b = name.toLowerCase().slice(0, query.length);
-    return a === b;
-  });
-
-  return hits;
-}
-
-function isRelevantProject(project: Project, tags: Tag[]): boolean {
-  const projectTagTypes = uniques(
-    project.tags.map((project) => project.category),
-  );
-
-  const commonTags: Tag[] = [];
-  projectTagTypes.forEach((category) => {
-    const projectTags = project.tags.filter((tag) => tag.category === category);
-    const tagsForType = tags.filter((tag) => tag.category === category);
-
-    const intersection = getIntersection(
-      projectTags,
-      tagsForType,
-      (a, b) => a.name === b.name,
-    );
-
-    commonTags.push(...intersection);
-  });
-
-  return commonTags.length > 0;
-}
-
-export function filterProjectsByTags(
-  projects: Project[],
-  tags: Tag[],
-): Project[] {
-  if (tags.length === 0) return projects;
-
-  const filtered = projects.filter((project) =>
-    isRelevantProject(project, tags),
-  );
-
-  return filtered;
-}
 
 export function getProjectsCountForTag(tag: Tag): number {
   return ALL_PROJECTS.filter((project) =>
-    project.tags.map((t) => t.name).includes(tag.name),
+    project.tags.filter(
+      (projectTag) =>
+        projectTag.category === tag.category && projectTag.name === tag.name,
+    ),
   ).length;
-}
-
-export function getProjectsCountForTagType(category: Tag['category']): number {
-  return ALL_PROJECTS.filter((project) =>
-    project.tags.map((tag) => tag.category).includes(category),
-  ).length;
-}
-
-export function useProjectsByTags(
-  tags: Tag[] | undefined,
-): SWRResponse<Project[], Error> {
-  const swr = useSWR<Project[], Error>(
-    tags
-      ? `projects?tags=${encodeURIComponent(
-          tags.map((tag) => tag.name).join(','),
-        )}`
-      : null,
-    async (): Promise<Project[]> => {
-      const result = await Promise.resolve(ALL_PROJECTS);
-
-      const filtered = filterProjectsByTags(result, tags ?? []);
-
-      return filtered;
-    },
-  );
-
-  return swr;
 }
