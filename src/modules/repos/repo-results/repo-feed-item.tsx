cdecdfc0f8204c1e5396ed2daa3d0000bb0f4bef
Lesley Chang <lesley.hanna98@gmail.com>
2022-02-27 11:40:49 -0500

    fix: follow git repo designs

--- src/modules/repos/repo-results/repo-feed-item.tsx
+++ src/modules/repos/repo-results/repo-feed-item.tsx
@@ -1,13 +1,10 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 
-import clsxm from '@/lib/utils/clsxm';
 import { GithubRepo } from '@/modules/github';
 
 const RepoStat = dynamic(() => import('@/ui/github/repo-stat'));
 const BasicOutlineBadge = dynamic(() => import('@/ui/basic-outline-badge'));
-
-const MAX_TOPICS_SHOWN = 5;
 
 export function RepoFeedItem({
   name,
@@ -15,39 +12,25 @@ export function RepoFeedItem({
   topics,
   starsCount,
 }: GithubRepo) {
-  const shouldShowRemainingTopicsInfo = topics.length - MAX_TOPICS_SHOWN > 0;
   return (
     <div className="bg-surface rounded bg-opacity-70">
       <div className="flex flex-col gap-2 px-3 py-3">
         <div className="text-xl font-semibold">{name}</div>
         <div className="text-base">{description}</div>
-        <ul className="flex flex-wrap items-center gap-1.5">
-          <li>
-            <RepoStat type="stargazers" count={starsCount} />
-          </li>
-        </ul>
         <div className="flex items-center gap-2">
           <ul className="flex flex-wrap items-center gap-1.5">
-            {topics.slice(0, MAX_TOPICS_SHOWN).map((topic) => (
+            {topics.map((topic) => (
               <li key={`${topic}`}>
                 <BasicOutlineBadge>{topic}</BasicOutlineBadge>
               </li>
             ))}
           </ul>
-          {shouldShowRemainingTopicsInfo && (
-            <button
-              className={clsxm(
-                'text-sm leading-none',
-                'flex h-6 min-w-[64px] items-center justify-center p-2',
-                'bg-transparent font-medium',
-                'text opacity-60 hover:font-semibold hover:opacity-80 active:opacity-100',
-                'transition-all',
-              )}
-            >
-              + {topics.length - MAX_TOPICS_SHOWN} more...
-            </button>
-          )}
         </div>
+        <ul className="flex flex-wrap items-center gap-1.5">
+          <li>
+            <RepoStat type="stargazers" count={starsCount} />
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/ui/basic-outline-badge.tsx
+++ src/ui/basic-outline-badge.tsx
@@ -8,7 +8,7 @@ const BasicOutlineBadge = ({ children, className, ...props }: any) => {
       className={clsxm(
         'text border border-basic-600 bg-basic-600 bg-opacity-10 text-basic-600',
         'h-5.5 rounded-full px-2.5 py-1',
-        'max-h-[24px] max-w-[140px] sm:max-w-[180px]',
+        'max-h-[24px] max-w-[180px] sm:max-w-[200px]',
         className,
       )}
       {...props}
