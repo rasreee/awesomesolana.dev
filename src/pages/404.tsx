fa47bba92d8afccfd660484bb1d05b2e0ad1c0e8
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-02-27 04:31:36 -0500

    feat: optimize 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,66 @@
-import { RiAlarmWarningFill } from 'react-icons/ri';
+import Link from 'next/link';
+import type { AnchorHTMLAttributes } from 'react';
 
 import { Seo } from '@/ui/components';
-import { ArrowLink } from '@/ui/links';
+import clsxm from '@/utils/clsxm';
+
+function ArrowLink({
+  children,
+  className,
+  direction = 'right',
+  href,
+  ...rest
+}: AnchorHTMLAttributes<HTMLAnchorElement> & {
+  direction?: 'left' | 'right';
+  href: string;
+}) {
+  return (
+    <Link href={href}>
+      <a
+        {...rest}
+        className={clsxm(
+          'animated-underline custom-link inline-flex items-center font-semibold',
+          'focus:outline-none focus-visible:rounded focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-offset-2',
+          'border-b border-dotted border-dark hover:border-black/0',
+          'group gap-[0.25em]',
+          direction === 'left' && 'flex-row-reverse',
+          className,
+        )}
+      >
+        <span>{children}</span>
+        <svg
+          viewBox="0 0 16 16"
+          height="1em"
+          width="1em"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={clsxm(
+            'relative',
+            'transition-transform duration-200',
+            direction === 'right' ? 'motion-safe:-translate-x-1' : 'rotate-180',
+            'group-hover:translate-x-0',
+          )}
+        >
+          <path
+            fill="currentColor"
+            d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"
+          />
+          <path
+            stroke="currentColor"
+            d="M1.75 8H11"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            className={clsxm(
+              'origin-left transition-all duration-200',
+              'opacity-0 motion-safe:-translate-x-1',
+              'group-hover:translate-x-0 group-hover:opacity-100',
+            )}
+          />
+        </svg>
+      </a>
+    </Link>
+  );
+}
 
 export default function NotFoundPage() {
   return (
@@ -9,10 +68,6 @@ export default function NotFoundPage() {
       <Seo title="Not Found" />
       <section className="bg-white">
         <div className="layout flex min-h-screen flex-col items-center justify-center text-center text-black">
-          <RiAlarmWarningFill
-            size={60}
-            className="drop-shadow-glow animate-flicker text-red-500"
-          />
           <h1 className="mt-8 text-4xl md:text-6xl">Page Not Found</h1>
           <ArrowLink className="mt-4 md:text-lg" href="/">
             Back to Home
--- src/ui/links/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './ArrowLink';
-export * from './UnderlineLink';
-export * from './UnstyledLink';
