3b4cde309f9cd8ca22a3ec55b1d46ed5c2b1efc2
Lesley Chang <lesley.hanna98@gmail.com>
2022-02-27 11:09:20 -0500

    feat: style results-info

--- src/modules/repos/repo-results/repos-results-info.tsx
+++ src/modules/repos/repo-results/repos-results-info.tsx
@@ -22,6 +22,8 @@ function getReposResultsInfoText({
   return result;
 }
 
+const MAX_TAGS_SHOWN = 3;
+
 export function ReposResultsInfo({
   data,
   params,
@@ -32,29 +34,50 @@ export function ReposResultsInfo({
   if (!data) return <div className="py-2 px-1">Loading...</div>;
 
   return (
-    <div className={clsxm('py-2 px-1', 'flex items-center gap-2')}>
-      <span className="text text-base leading-none opacity-80">
+    <div
+      className={clsxm(
+        'px-1 py-3',
+        'flex flex-col gap-2 sm:flex-row sm:items-center',
+      )}
+    >
+      <span className="text-body text-sm leading-tight sm:text-base">
         {getReposResultsInfoText({
           count: data.items.length,
           totalCount: data.totalCount,
           params,
         })}
       </span>
-      <span className="flex items-center gap-2">
-        {params?.tags?.map((filter) => (
-          <Badge
-            key={filter.type + '_' + filter.name}
+      <div className="flex w-full items-center justify-between">
+        <span className="flex max-w-[60%] items-center gap-1.5">
+          {params?.tags?.slice(0, MAX_TAGS_SHOWN).map((filter) => (
+            <Badge
+              key={filter.type + '_' + filter.name}
+              className={clsxm(
+                'text border border-green-500 bg-green-500 bg-opacity-10 text-green-500',
+                'h-5.5 rounded-full px-2.5 py-1',
+                'max-h-[24px] max-w-[140px]',
+              )}
+            >
+              <span className="truncate text-sm leading-none">
+                {filter.name}
+              </span>
+            </Badge>
+          ))}
+        </span>
+        {params?.tags && params.tags.length - MAX_TAGS_SHOWN > 0 && (
+          <button
             className={clsxm(
-              'text text-sm leading-none',
-              'text border border-green-500 bg-green-500 bg-opacity-10 text-green-500',
-              'h-5.5 rounded-full px-2.5 py-1',
-              'flex items-center justify-center',
+              'text-[15px] leading-none',
+              'flex h-6 min-w-[64px] items-center justify-center p-2',
+              'bg-transparent font-medium',
+              'text opacity-80 hover:opacity-90 active:opacity-100',
             )}
+            onClick={() => console.log('')}
           >
-            {filter.name}
-          </Badge>
-        ))}
-      </span>
+            + {params.tags.length - MAX_TAGS_SHOWN} more...
+          </button>
+        )}
+      </div>
     </div>
   );
 }
