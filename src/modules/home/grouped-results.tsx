faff63270e3faa1fd7d53c3e0fd54ba14a44387c
Lesley Chang <lesley.hanna98@gmail.com>
2022-02-27 14:52:29 -0500

    refactor: grouped results

--- src/modules/home/grouped-results.tsx
+++ src/modules/home/grouped-results.tsx
@@ -1,3 +1,4 @@
+import { computed } from 'mobx';
 import { observer } from 'mobx-react-lite';
 import { useRouter } from 'next/router';
 
@@ -8,25 +9,23 @@ import { Tag, TagType, tagTypes } from '@/modules/tags';
 import { useTagsSearchStore } from '@/modules/tags/tags-search-store';
 import Popover from '@/ui/popover';
 
-type GroupedHits = Array<{ type: TagType; hits: Tag[] }>;
+type TagGroup = { type: TagType; tags: Tag[] };
+
+type GroupedHits = Array<TagGroup>;
 
 function groupByTag(list: Tag[]): GroupedHits {
   const groups = tagTypes.map((type) => ({
     type,
-    hits: list.filter((tag) => tag.type === type),
+    tags: list.filter((tag) => tag.type === type),
   }));
 
   return groups;
 }
 
 const GroupedResults = observer(function GroupedResults() {
-  const router = useRouter();
   const tagsSearchStore = useTagsSearchStore();
 
-  const handleTagClick = (tag: Tag) => () => {
-    router.push(appRoute.repos.search({ tags: [tag] }));
-    tagsSearchStore.onReset();
-  };
+  const tagGroups = computed(() => groupByTag(tagsSearchStore.hits)).get();
 
   return (
     <Popover
@@ -34,30 +33,42 @@ const GroupedResults = observer(function GroupedResults() {
       isOpen={!!tagsSearchStore.hits.length}
       onClose={tagsSearchStore.onReset}
     >
-      {groupByTag(tagsSearchStore.hits).map(
-        ({ type, hits: list }) =>
-          list.length > 0 && (
-            <div className="flex flex-col gap-2 px-1" key={type}>
-              <span className="px-3 py-2 text-lg font-semibold">
-                {capitalize(pluralize(type))} {`(${list.length})`}
-              </span>
-              <ul className="max-h-[16rem] overflow-y-auto">
-                {list.map((hit) => (
-                  <li className="w-full" key={`${hit.type}_${hit.name}`}>
-                    <button
-                      className="hover:bg-surface-1 w-full rounded-md py-3 px-3 text-left"
-                      onClick={handleTagClick(hit)}
-                    >
-                      {hit.name}
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ),
+      {tagGroups.map(
+        ({ type, tags }) =>
+          tags.length > 0 && <GroupTags key={type} type={type} tags={tags} />,
       )}
     </Popover>
   );
 });
+
+const GroupTags = ({ type, tags }: TagGroup) => {
+  const router = useRouter();
+  const tagsSearchStore = useTagsSearchStore();
+
+  const handleSelect = (tag: Tag) => () => {
+    router.push(appRoute.repos.search({ tags: [tag] }));
+    tagsSearchStore.onReset();
+  };
+
+  return (
+    <div className="flex flex-col gap-2 px-1" key={type}>
+      <span className="px-3 py-2 text-lg font-semibold">
+        {capitalize(pluralize(type))} {`(${tags.length})`}
+      </span>
+      <ul className="max-h-[16rem] overflow-y-auto">
+        {tags.map((tag) => (
+          <li className="w-full" key={`${tag.type}_${tag.name}`}>
+            <button
+              className="hover:bg-surface-1 w-full rounded-md py-3 px-3 text-left"
+              onClick={handleSelect(tag)}
+            >
+              {tag.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default GroupedResults;
